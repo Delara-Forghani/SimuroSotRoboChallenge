2ce2d95625449cc6292389bffaa67fff0c82f30f
Delara-Forghani <Del.Forghani@gmail.com>
2018-08-06 19:11:35 +0430

    some changes in vision.py

--- img_processing/scripts/vision.py
+++ img_processing/scripts/vision.py
@@ -7,44 +7,52 @@ import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from system_messages.msg import Coordination
+from system_messages.msg import Rectangle
+from system_messages.srv import PointsOfInterest
 import time
 from geometry import Point
 from obstacleBorder import *
 
 
 class Vision:
+
     def __init__(self):
         self.image=None
-        self.obstacle_boundaries=list()
         self.bridge = CvBridge()
-        self.left_top_point=Coordination()
-        self.right_top_point=Coordination()
-        self.left_bot_point=Coordination()
-        self.right_bot_point=Coordination()
-        self.left_flag = False
-        self.right_flag = False
-        self.state = 0
-        self.cmd = 0
+        self.camera_subscriber=None
+        self.srv=None
+        self.yellow_goal=True
+        self.obstacle_positions=list()
+        self.goal_position=Rectangle()
+        self.obstacle_border=Rectangle()
+        self.no_goal_found=False
     def set_pubs_subs(self):
         self.camera_subscriber = rospy.Subscriber("/camera/rgb/image_raw",Image,self.image_callback)
-        self.goal_point_pub = rospy.Publisher("/target/coordination",Coordination,queue_size=1)
-        self.target_point_sub=rospy.Subscriber("/target/coordination",Coordination,self.coordinate_callback)
-        self.pub_state = rospy.Publisher('/state', std_msgs.msg.Int8, queue_size=10)
+        self.srv=rospy.Service('points_of_interest',PointsOfInterest,self.service_callback)
+
+    def service_callback(self,point):
+        print(point.req.requsetNum)
+        if(point.req.pick_yellow_goal == False):
+            self.yellow_goal = False
+        else:
+            self.yellow_goal=True
+        # del self.obstacle_positions[:]
+        # self.setImageToCV()
+        point.res.obstacle_positions=self.obstacle_positions
+        point.res.goal_position=self.goal_position
+        point.res.no_goal_found=self.no_goal_found
 
 
     def coordinate_callback(self,):
         print("target point received")
 
+
     def image_callback(self,image):
         #print("image received")
-        # for i in range len(self.obstacle_boundaries):
-        #     self.obstacle_boundaries=list()
-
-        del self.obstacle_boundaries[:]
-        #print(len(self.obstacle_boundaries))
         self.image=image
+        del self.obstacle_positions[:]
         self.setImageToCV()
-        self.pub_state.publish(self.cmd)
+
 
 
     def setImageToCV(self):
@@ -55,18 +63,23 @@ class Vision:
         except CvBridgeError as e:
             print(e)
 
-    #def distance(self , )
+
 
 
     def detectOpenCVImage(self,cv_image):
         #print("detectOpenCVImage")
-        im =cv_image[360:480 , 165:475]
+        im=cv_image
+        crop_im=cv_image[360:480 , 165:475]
         frame = cv2.GaussianBlur(im, (3, 3), 0)
+        crop_frame=cv2.GaussianBlur(crop_im, (3, 3), 0)
 
 
         # Switch image from BGR colorspace to HSV
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        crop_hsv=hsv[360:480 , 165:475]
+        crop_gray=img_gray[360:480 , 165:475]
+
 
 
 
@@ -74,81 +87,47 @@ class Vision:
         yellowMin = np.array([20, 100, 10])
         yellowMax =  np.array([30, 255, 255])
 
-        #blueMin = np.array([90, 100, 10])        it may be interchanged with yellow
-        #blueMax =  np.array([125, 255, 255])     it may be interchanged with yellow
-
+        blueMin = np.array([90, 100, 10])
+        blueMax =  np.array([125, 255, 255])
 
         redMin= np.array([170, 100, 10])
         redMax= np.array([180, 255, 255])
-        # Sets pixels to white if in purple range, else will be set to black
 
 
-
-        greenMin=np.array([40,100,10])
-        greenMax=np.array([90,255,255])
-
-
-        grayMin=np.array([18,4,39])
-        grayMax=np.array([28,10,100])
-
-
-
-
-
-        #blue_mask = cv2.inRange(hsv, blueMin, blueMax)      it may be interchanged with yellow
+        blue_mask = cv2.inRange(hsv, blueMin, blueMax)
         yellow_mask = cv2.inRange(hsv, yellowMin, yellowMax)
-        red_mask = cv2.inRange(hsv, redMin, redMax)
-        green_mask=cv2.inRange(hsv,greenMin , greenMax)
-        gray_mask =cv2.inRange(hsv,grayMin,grayMax)
-
-
-
-
-        yellow_result = cv2.bitwise_and(frame, frame, mask=yellow_mask)
-        red_result = cv2.bitwise_and(frame, frame, mask=red_mask)
-        green_result=cv2.bitwise_and(frame , frame ,mask=green_mask)
-        gray_result=cv2.bitwise_and(frame , frame , mask=gray_mask)
-        red_green_mask=cv2.bitwise_or(red_mask , green_mask)
-        red_green_result=cv2.bitwise_and(frame ,frame ,mask=red_green_mask)
+        red_mask = cv2.inRange(crop_hsv, redMin, redMax)
 
 
         # dilate makes the in range areas larger
+        blue_mask = cv2.dilate(blue_mask, None, iterations=1)
         yellow_mask = cv2.dilate(yellow_mask, None, iterations=1)
         red_mask = cv2.dilate(red_mask, None, iterations=1)
-        red_green_mask=cv2.dilate(red_green_mask , None ,iterations=1)
 
 
-        edges = cv2.Canny(red_mask, 100, 200)
+
+        blue_result = cv2.bitwise_and(frame,frame, mask=blue_mask)
+        yellow_result = cv2.bitwise_and(frame,frame, mask=yellow_mask)
+        red_result = cv2.bitwise_and(crop_frame, crop_frame, mask=red_mask)
+
+
+        blue_to_gray=cv2.cvtColor(blue_result,cv2.COLOR_BGR2GRAY)
         yellow_to_gray=cv2.cvtColor(yellow_result,cv2.COLOR_BGR2GRAY)
         red_to_gray = cv2.cvtColor(red_result, cv2.COLOR_BGR2GRAY)
-        redgreen_to_gray = cv2.cvtColor(red_green_result,cv2.COLOR_BGR2GRAY)
-        (yellow_ret, yellow_thresh) = cv2.threshold(yellow_to_gray, 50, 200, cv2.THRESH_BINARY)
+        (blue_ret, blue_thresh) = cv2.threshold(blue_to_gray, 50 , 255, cv2.THRESH_BINARY)
+        (yellow_ret, yellow_thresh) = cv2.threshold(yellow_to_gray, 70, 200, cv2.THRESH_BINARY)
         (red_ret, red_thresh) = cv2.threshold(red_to_gray, 10, 255, cv2.THRESH_BINARY)
-        (rg_ret ,rg_thresh) = cv2.threshold(redgreen_to_gray,50,255,cv2.THRESH_BINARY)
         im1,red_contours, hierarchy1 = cv2.findContours(red_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         im2,yellow_contours, hierarchy2 = cv2.findContours(yellow_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        im3,rg_contours, hierarchy3 = cv2.findContours(rg_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        im3,blue_contours, hierarchy3 = cv2.findContours(blue_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 
-
-
-
-        if(len(rg_contours) > 0):
-            cnt=max(rg_contours , key=cv2.contourArea)
-            cv2.drawContours(red_green_result,[cnt], -1, (0, 255, 0), 3)
         if(len(red_contours)> 0):
             cnt = max(red_contours, key=cv2.contourArea)
             red_contours.sort(key=cv2.contourArea)
 
             #print("here is red_contours :" , len(red_contours))
             for i in red_contours:
-                hull = cv2.convexHull(i)
-                cv2.drawContours(red_result, [hull], -1, (0, 0, 255), 1)
-                epsilon = 0.01 * cv2.arcLength(i, True)
-                approx = cv2.approxPolyDP(i, epsilon, True)
-                cv2.drawContours(red_result, [approx], -1, (255,0, 0), 1)
-
-
 
 
                 rect = cv2.boundingRect(i)
@@ -156,60 +135,82 @@ class Vision:
                 cv2.rectangle(red_result,(x,y),(x+w,y+h),(255,0,0),2)
 
 
-
-
-                left_top=Point(x,y)
-                right_top=Point(x+w,y)
-                left_bot=Point(x,y+h)
-                right_bot=Point(x+w,y+h)
-                border=Border(left_top,right_top,left_bot,right_bot)
-                self.obstacle_boundaries.append(border)
-                border.print_points()
-
-
                 # ****************
-                self.left_top_point.xCoordinate.data=left_top.x
-                self.left_top_point.yCoordinate.data=left_top.y
+                self.obstacle_border.left_top_coord.x=x
+                self.obstacle_border.left_top_coord.y=y
 
-                self.right_top_point.xCoordinate.data=right_top.x
-                self.right_top_point.yCoordinate.data=right_top.y
+                self.obstacle_border.right_top_coord.x=x+w
+                self.obstacle_border.right_top_coord.y=y
 
-                self.left_bot_point.xCoordinate.data=left_bot.x
-                self.left_bot_point.yCoordinate.data=left_bot.y
+                self.obstacle_border.left_bot_coord.x=x
+                self.obstacle_border.left_bot_coord.y=y+h
 
-                self.right_bot_point.xCoordinate.data=right_bot.x
-                self.right_bot_point.yCoordinate.data=right_bot.y
-                print("*******")
-                print((self.left_top_point.xCoordinate.data , self.left_top_point.yCoordinate.data))
+                self.obstacle_border.right_bot_coord.x=x+w
+                self.obstacle_border.right_bot_coord.y=y+h
 
-            #print(len(self.obstacle_boundaries))
-            for i in range (0 , len(self.obstacle_boundaries)):
-                if(self.obstacle_boundaries[i].get_right_bot().x == 310 ):
-                    self.right_flag = True
-                if(self.obstacle_boundaries[i].get_left_bot().x == 0 ):
-                    self.left_flag= True
+                self.obstacle_positions.append(self.obstacle_border)
+        #print(len(self.obstacle_positions))
+        if(self.yellow_goal == True):
+            if(len(yellow_contours) == 0):
+                self.no_goal_found=True
+            elif(len(yellow_contours) > 0):
+                yellow_cnt = max(yellow_contours, key=cv2.contourArea)
+                if(cv2.contourArea(yellow_cnt) == 0):
+                    self.no_goal_found=True
 
+                else:
+                    self.no_goal_found=False
+                    rect = cv2.boundingRect(yellow_cnt)
+                    x,y,w,h = rect
+                    cv2.rectangle(yellow_result,(x,y),(x+w,y+h),(255,0,0),2)
+                    self.goal_position.left_top_coord.x=x
+                    self.goal_position.left_top_coord.y=y
 
-            if(self.right_flag == True and self.left_flag == True):
-                self.cmd = 0
-            elif(self.right_flag == True and self.left_flag == False):
-                self.cmd = 1
-            elif(self.right_flag == False and self.left_flag == True):
-                self.cmd = 2
-            elif(self.right_flag == False and self.left_flag == False):
-                self.cmd = 3
-            print(self.cmd)
+                    self.goal_position.right_top_coord.x=x+w
+                    self.goal_position.right_top_coord.y=y
 
-        cv2.drawContours(yellow_result,yellow_contours, -1, (0, 255, 0), 3)
+                    self.goal_position.left_bot_coord.x=x
+                    self.goal_position.left_bot_coord.y=y+h
 
+                    self.goal_position.right_bot_coord.x=x+w
+                    self.goal_position.right_bot_coord.y=y+h
+        # print(self.no_goal_found)
+        # print((self.goal_position.left_top_coord.x , self.goal_position.left_top_coord.y) , (self.goal_position.right_top_coord.x , self.goal_position.right_top_coord.y) ,
+        # (self.goal_position.left_bot_coord.x , self.goal_position.left_bot_coord.y) , (self.goal_position.right_bot_coord.x , self.goal_position.right_bot_coord.y))
 
+        if(self.yellow_goal == False):
+            if(len(blue_contours) == 0):
+                self.no_goal_found = True
+            elif(len(blue_contours) > 0):
+                blue_cnt = max(blue_contours, key=cv2.contourArea)
+                if(cv2.contourArea(blue_cnt) == 0):
+                    self.no_goal_found=True
+                else:
+                    self.no_goal_found=False
+                    rect = cv2.boundingRect(blue_cnt)
+                    x,y,w,h = rect
+                    cv2.rectangle(blue_result,(x,y),(x+w,y+h),(255,255,0),2)
+                    self.goal_position.left_top_coord.x=x
+                    self.goal_position.left_top_coord.y=y
 
+                    self.goal_position.right_top_coord.x=x+w
+                    self.goal_position.right_top_coord.y=y
+
+                    self.goal_position.left_bot_coord.x=x
+                    self.goal_position.left_bot_coord.y=y+h
+
+                    self.goal_position.right_bot_coord.x=x+w
+                    self.goal_position.right_bot_coord.y=y+h
 
 
 
 
         cv2.imshow('frame',frame)
+        cv2.imshow('crop_frame',crop_frame)
         cv2.imshow('red_result', red_result)
+        cv2.imshow('blue_result', blue_result)
+        cv2.imshow('yellow_result', yellow_result)
+
 
         k = cv2.waitKey(1)
 
